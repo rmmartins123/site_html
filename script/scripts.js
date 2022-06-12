dae7515b30e55d7955bf2320b14c7ac71a9ca608
rmmartins123 <62813752+rmmartins123@users.noreply.github.com>
2022-06-12 05:59:13 -0300

    Update scripts.js

--- script/scripts.js
+++ script/scripts.js
@@ -1,25 +1,26 @@
+const newLocal = "selector";
 function calcular(n1,n2) {
     n1 = parseFloat(document.getElementById("n1").value)
     n2 = parseFloat(document.getElementById("n2").value)
 
-    selector = document.getElementById("selector").value
+    selector = document.getElementById(newLocal).value
 
     switch(selector) {
         case '+':
             calculo = (n1 + n2)
-            document.getElementById("resultado").innerHTML = `O resultado da soma de ${n1} + ${n2} é = ${calculo}`
+            document.getElementById("resultado").innerHTML = `O resultado da soma de ${n1} + ${n2} é = ${calculo}`;
             break
         case '-':
             calculo = (n1 - n2)
-            document.getElementById("resultado").innerHTML = `O resultado da diminuição de ${n1} - ${n2} é = ${calculo}`
+            document.getElementById("resultado").innerHTML = `O resultado da diminuição de ${n1} - ${n2} é = ${calculo}`;
             break
         case '*':
             calculo = (n1 * n2)
-            document.getElementById("resultado").innerHTML = `O resultado da multiplicação de ${n1} * ${n2} é = ${calculo}`
+            document.getElementById("resultado").innerHTML = `O resultado da multiplicação de ${n1} * ${n2} é = ${calculo}`;
             break
         case '/':
             calculo = Math.round((n1 / n2))
-            document.getElementById("resultado").innerHTML = `O resultado da divisão de ${n1} / ${n2} é = ${calculo}`
+            document.getElementById("resultado").innerHTML = `O resultado da divisão de ${n1} / ${n2} é = ${calculo}`;
     }
 
 }
